cc9d24fc5da16a1ee97fbe515441ce89f111419a
Phyo Htut <pkthein@gmail.com>
2021-01-07 21:04:46 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+
+// import Day1 from './components/Day1'
+import Todo from './components/Todo'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Day1></Day1> */}
+      <Todo></Todo>
     </div>
   );
 }
